e42940e7864e5217d1459c94b631b549db6fece3
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-06-17 15:09:06 -0700

    resources endpoints finsihed

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,25 @@
-// build your `/api/resources` router here
+const express = require('express')
+const Resource = require('./model')
+const { validateName } = require('./middleware')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const resources = await Resource.get()
+        res.json(resources)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', validateName, async (req, res, next) => {
+    try {
+        const newResource = await Resource.create(req.body)
+        res.status(201).json(newResource)
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@
 const express = require('express')
 
 const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
 
 const server = express()
 
 server.use(express.json())
 
 server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
 
 module.exports = server
