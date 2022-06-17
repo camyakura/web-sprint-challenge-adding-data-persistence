6ed68eed029d0e44f3a669b9ce913bc8c93fe91a
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-06-17 14:07:48 -0700

    completed project endpoints

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,22 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig')
+
+function get() {
+    return db('projects')
+}
+
+function getById(id) {
+    return db('projects').where('project_id', id).first()
+}
+
+function create (project) {
+    return db('projects').insert(project)
+        .then(([id]) => { 
+            return getById(id)
+        })
+}
+
+module.exports = {
+    get,
+    getById,
+    create,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,30 @@
 // build your `/api/projects` router here
+const express = require('express')
+const Project = require('./model')
+const { validateName } = require('./middleware')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const projects = await Project.get()
+        const updateBoolean = projects.map(project => {
+            return {...project, project_completed: project.project_completed ? true : false}
+        })
+        res.json(updateBoolean)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', validateName, async (req, res, next) => {
+    try {
+        const newProject = await Project.create(req.body)
+        res.status(201).json({...newProject, project_completed: newProject.project_completed ? true : false})
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+module.exports = router
